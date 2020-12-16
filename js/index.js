ae65f8b3e8a374585b79fa317545695d369bbd08
cesarvidarte <cesarvidarte666@hotmail.com>
2020-12-16 14:30:16 -0500

    usando grunt

--- js/index.js
+++ js/index.js
@@ -3,5 +3,23 @@ $(function(){
   $("[data-toggle='popover']").popover();
   $('.carousel').carousel({
     interval: 5000
-  })
-})
+  });
+  $('#contacto').on('show.bs.modal', function (e) {
+    console.log('el modal se está mostrando');
+    $('#contactoBtn').removeClass('btn-outline-success');
+    $('#contactoBtn').addClass('btn-primary');
+    $('#contactoBtn').prop('disabled',true);
+  });
+  $('#contacto').on('shown.bs.modal', function (e) {
+    console.log('el modal se mostó');
+  });
+  $('#contacto').on('hide.bs.modal', function (e) {
+    console.log('el modal se oculta');
+  });
+  $('#contacto').on('hidden.bs.modal', function (e) {
+    console.log('el modal se ocultó');
+    $('#contactoBtn').addClass('btn-outline-success');
+    $('#contactoBtn').removeClass('btn-primary');
+    $('#contactoBtn').prop('disabled',false);
+  });
+});
